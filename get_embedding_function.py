78ef67f641089ee89d214b26a2abce25234e2ef9
William Chung <williamchung@ucsb.edu>
2026-03-30 22:11:46 -0700

    update depreciated modules, add data

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,4 +1,4 @@
-from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_ollama import OllamaEmbeddings
 from langchain_community.embeddings.bedrock import BedrockEmbeddings
 
 
